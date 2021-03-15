665e588c3e50b1109a1bc6f0550fb4edd2753cc6
kirill.kapustin <kirill.kapustin@intexsoft.by>
2021-03-15 18:30:06 +0300

    add ariphmetic actions func

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,25 @@ import CalcButtons from "./components/calc-buttons/CalcButtons";
 function App() {
   const [currentValue, setCurrentValue] = useState('0');
   const [currentExpression, setCurrentExpression] = useState('0');
+  const [previousValue, setPreviousValue] = useState('');
+  const [currentOperand, setCurrentOperand] = useState('');
+  const [nextValue, setNextValue] = useState(false);
 
   return (
     <StyledAppWrapper>
       <StyledCalcWrapper>
         <Display expression={currentExpression !== '0' ? currentExpression : ''} currentValue={currentValue}/>
-        <CalcButtons setCurrent={setCurrentValue} setExpression={setCurrentExpression} currentValue={currentValue}/>
+        <CalcButtons
+          setCurrent={setCurrentValue}
+          setExpression={setCurrentExpression}
+          setOperand={setCurrentOperand}
+          setPreviousValue={setPreviousValue}
+          setNext={setNextValue}
+          currentOperand={currentOperand}
+          currentValue={currentValue}
+          nextValue={nextValue}
+          previousValue={previousValue}
+        />
       </StyledCalcWrapper>
     </StyledAppWrapper>
   );
--- src/components/calc-buttons/CalcButtons.tsx
+++ src/components/calc-buttons/CalcButtons.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { StyledCalcButtons } from './StyledCalcButtons';
-import Button from "../button/Button";
-import { largeButton, lowFuncButton } from "../../core/styles";
+import Button from '../button/Button';
+import { largeButton, lowFuncButton } from '../../core/styles';
 
 interface ICalc {
   setCurrent: any;
   setExpression: any;
+  setOperand: any;
+  setPreviousValue: any;
+  setNext: any;
+  currentOperand: string;
   currentValue: string;
+  nextValue: boolean;
+  previousValue: string;
 }
 
-const CalcButtons: React.FC<ICalc> = ({setCurrent, setExpression, currentValue}) => {
+const CalcButtons: React.FC<ICalc> = ({
+  setCurrent,
+  setExpression,
+  setOperand,
+  setPreviousValue,
+  setNext,
+  nextValue,
+  previousValue,
+  currentOperand,
+  currentValue}) => {
+
   const onNumberClick = (e: any) => {
-    if (currentValue === '0') {
+    if (currentOperand !== '' && nextValue) {
+      setCurrent(e.target.innerText);
+      setNext('');
+    } else if (currentValue === '0') {
       setCurrent(e.target.innerText);
     } else {
       currentValue.length < 11 && currentValue[0] === '-' && setCurrent(currentValue.concat(e.target.innerText));
@@ -19,7 +38,13 @@ const CalcButtons: React.FC<ICalc> = ({setCurrent, setExpression, currentValue})
     }
   };
 
-  const onClear = () => setCurrent('0');
+  const onClear = () => {
+    setCurrent('0');
+    setExpression('0');
+    setOperand('');
+    setPreviousValue('');
+    setNext(false);
+  };
 
   const onPlusMinus = () => {
     currentValue[0] === '-' && currentValue !== '0' && setCurrent(currentValue.slice(1));
@@ -33,24 +58,89 @@ const CalcButtons: React.FC<ICalc> = ({setCurrent, setExpression, currentValue})
     isPoint === -1 && setCurrent(currentValue.concat(','));
   };
 
+  const onCalcAction = (e: any) => {
+    switch (e.target.innerText) {
+      case '+' :
+        if (currentOperand === '+') {
+          setOperand('+');
+          const result = parseFloat(previousValue) + parseFloat(currentValue);
+          setPreviousValue(result.toString());
+          setCurrent(result.toString());
+          setExpression(`${result.toString()} + `);
+          setNext(true);
+        } else {
+          setOperand('+');
+          setPreviousValue(currentValue);
+          setExpression(`${currentValue} + `);
+          setNext(true);
+        }
+        break;
+      case '-' :
+        if (currentOperand === '-') {
+          setOperand('-');
+          const result = parseFloat(previousValue) - parseFloat(currentValue);
+          setPreviousValue(result.toString());
+          setCurrent(result.toString());
+          setExpression(`${result.toString()} - `);
+          setNext(true);
+        } else {
+          setOperand('-');
+          setPreviousValue(currentValue);
+          setExpression(`${currentValue} - `);
+          setNext(true);
+        }
+        break;
+      case '/' :
+        if (currentOperand === '/') {
+          setOperand('/');
+          const result = parseFloat(previousValue)/parseFloat(currentValue);
+          setPreviousValue(result.toString());
+          setCurrent(result.toString());
+          setExpression(`${result.toString()} / `);
+          setNext(true);
+        } else {
+          setOperand('/');
+          setPreviousValue(currentValue);
+          setExpression(`${currentValue} / `);
+          setNext(true);
+        }
+        break;
+      case '*' :
+        if (currentOperand === '*') {
+          setOperand('*');
+          const result = parseFloat(previousValue) * parseFloat(currentValue);
+          setPreviousValue(result.toString());
+          setCurrent(result.toString());
+          setExpression(`${result.toString()} * `);
+          setNext(true);
+        } else {
+          setOperand('*');
+          setPreviousValue(currentValue);
+          setExpression(`${currentValue} * `);
+          setNext(true);
+        }
+        break;
+    }
+  };
+
   return (
     <StyledCalcButtons>
       <Button style={lowFuncButton} onClick={onClear}>C</Button>
       <Button style={lowFuncButton} onClick={onPlusMinus}>+/-</Button>
       <Button style={lowFuncButton} onClick={onPercent}>%</Button>
-      <Button>/</Button>
+      <Button onClick={onCalcAction}>/</Button>
       <Button onClick={onNumberClick}>7</Button>
       <Button onClick={onNumberClick}>8</Button>
       <Button onClick={onNumberClick}>9</Button>
-      <Button>*</Button>
+      <Button onClick={onCalcAction}>*</Button>
       <Button onClick={onNumberClick}>4</Button>
       <Button onClick={onNumberClick}>5</Button>
       <Button onClick={onNumberClick}>6</Button>
-      <Button>-</Button>
+      <Button onClick={onCalcAction}>-</Button>
       <Button onClick={onNumberClick}>1</Button>
       <Button onClick={onNumberClick}>2</Button>
       <Button onClick={onNumberClick}>3</Button>
-      <Button>+</Button>
+      <Button onClick={onCalcAction}>+</Button>
       <Button style={largeButton} onClick={onNumberClick}>0</Button>
       <Button onClick={onPoint}>.</Button>
       <Button>=</Button>
