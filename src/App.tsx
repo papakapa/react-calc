46c4bf01d25b1606fbcc45d286260938be492d24
kirill.kapustin <kirill.kapustin@intexsoft.by>
2021-03-15 10:17:07 +0300

    calc mask layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-wrapper">
+      <div className="calc-wrapper">
+        <div className='calc-display'>
+
+        </div>
+        <div className='calc-buttons'>
+          <button>C</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>/</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>*</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
